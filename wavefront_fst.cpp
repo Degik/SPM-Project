e278a95b5542196750d83ae06c9ec6bf9dfdc32c
Degik <prodax9900@gmail.com>
2024-08-29 12:40:58 +0200

    Update wavefront_fst.cpp

--- wavefront_fst.cpp
+++ wavefront_fst.cpp
@@ -11,6 +11,7 @@
 #include <list>
 
 #include <ff/ff.hpp>
+#include <ff/farm.hpp>
 #include <ff/parallel_for.hpp>
 #include <ff/pipeline.hpp>
 
@@ -56,7 +57,7 @@ Resources CalculateResources(uint16_t W, uint16_t K, uint16_t N){
     \brief Fill the matrix M with the values
     \note Fill the matrix M with the values
 */
-vector<vector<double>>& FillMatrix(vector<vector<double>>& M, uint16_t N, uint16_t W){
+matrix_d& FillMatrix(matrix_d& M, uint16_t N, uint16_t W){
     ParallelFor pf(W); // Create the parallel for object with W workers
     // Fill the diagonal elements (i,j) (where i == j) with (m+1)/N
     pf.parallel_for(0, N, [&](const long m){
@@ -71,7 +72,7 @@ vector<vector<double>>& FillMatrix(vector<vector<double>>& M, uint16_t N, uint16
     \brief Print the matrix M
     \note Print the matrix M
 */
-void PrintMatrix(vector<vector<double>>& M, uint16_t N){
+void PrintMatrix(matrix_d& M, uint16_t N){
     for(int i = 0; i < N; i++){
         for(int j = 0; j < N; j++){
             cout << M[i][j] << " ";
@@ -85,7 +86,7 @@ void PrintMatrix(vector<vector<double>>& M, uint16_t N){
     \brief Save the matrix M to a file
     \note Save the matrix M to a file with the name filename
 */
-void SaveMatrixToFile(vector<vector<double>>& M, uint16_t N, string filename){
+void SaveMatrixToFile(matrix_d& M, uint16_t N, string filename){
     ofstream file;
     file.open(filename);
     for(int i = 0; i < N; i++){
@@ -142,16 +143,17 @@ list<vector_d> SplitVector(vector_d v, uint16_t D, uint16_t K){
     \brief Create the matrix M
     \note Create the matrix M with the size N and fill it with the values
 */
-struct CreateMatrix: ff_node_t<int, vector<vector<double>>> {
+struct CreateMatrix: ff_node_t<int, matrix_d> {
     uint16_t N, W;
     CreateMatrix(uint16_t size, uint16_t workers): N(size), W(workers) {} // Constructor
     
-    vector<vector<double>> *svc(int *task){ // Service
-        vector<vector<double>> M (N, vector<double>(N, 0.0)); // Create the matrix
+    matrix_d *svc(int *task){ // Service
+        matrix_d M (N, vector_d(N, 0.0)); // Create the matrix
         M = FillMatrix(M, N, W);     // Fill the matrix
         PrintMatrix(M, N);           // Print the matrix
         SaveMatrixToFile(M, N, "matrix.txt");      // Save the matrix to a file
-        return new vector<vector<double>>(M);
+        ff_send_out(new matrix_d(M));             // Send the matrix to the next stage
+        return GO_ON;
     }
     
 };
@@ -182,6 +184,75 @@ struct Sink: ff_minode_t<double, double>{
     }
 };
 
+/*!
+    \name DotProduct_Emitter
+    \brief Split the vectors v1 and v2 for the workers
+    \note Split the vectors v1 and v2 for the workers
+*/
+struct DotProduct_Emitter: ff_node_t<tuple<vector_d, vector_d, int>>{
+    uint16_t K, W, D;
+    const vector_d& v1, v2;
+    
+    DotProduct_Emitter(uint16_t K, uint16_t W, uint16_t D, const vector_d& v1, const vector_d& v2)
+        : K(K), W(W), D(D), v1(v1), v2(v2) {}
+    
+    tuple<vector_d, vector_d, int>* svc(tuple<vector_d, vector_d, int>*){
+        //
+        list<vector_d> sub_v1_list(D);
+        list<vector_d> sub_v2_list(D);
+        //
+        // if K is less or equal with D we can have 1:(1,1) 
+        // [one workers:for one elememt] for calculate the dot product
+        sub_v1_list = SplitVector(v1, D, K);
+        sub_v2_list = SplitVector(v2, D, K);
+        //
+        auto begin_v1 = sub_v1_list.begin();
+        auto begin_v2 = sub_v2_list.begin();
+        for(uint16_t w = 0; w < D; w++, begin_v1++, begin_v2++){
+            // Take the sub-vectors and send them to the workers
+            const uint16_t size = begin_v1->size(); // v1 and v2 have the same size
+            ff_send_out(new tuple<vector_d, vector_d, int>(*begin_v1, *begin_v2, size));
+            cout << "Tuple sent (DotProduct)" << endl;
+        }
+
+        return EOS;
+    }
+};
+
+/*!
+    \name Diagonal_Emitter
+    \brief Take the matrix M and split it for the workers
+    \note Take the matrix M and split it for the workers
+*/
+struct Diagonal_Emitter: ff_node_t<tuple_dot_product>{
+    matrix_d& M;
+    uint16_t N, K, W, D;
+
+    Diagonal_Emitter(matrix_d& M, uint16_t N, uint16_t K, uint16_t W, uint16_t D)
+        : M(M), N(N), K(K), W(W), D(D) {}
+
+    tuple_dot_product* svc(tuple_dot_product*){
+        cout << "m = [0, " << N-K << "[" << endl;
+        for (int m = 0; m < N - K; m++){
+            cout << "Taking v1 and v2 vectors for m: " << m << endl;
+            vector_d v1;
+            vector_d v2;
+            for (int i = 0; i < K; i++){
+                v1.push_back(M[m][i]);
+                cout << "M[" << m << "][" << i << "]: " << M[m][i] << endl;
+            }
+            for (int i = 0; i < K; i++){
+                v2.push_back(M[m+K][i]);
+                cout << "M[" << i << "][" << m+K << "]: " << M[i][m+K] << endl;
+            }
+            cout << "Sending the tuple to the farm" << endl;
+            ff_send_out(new tuple_dot_product(v1, v2, M, K, W, D, m, m+K));
+            cout << "Tuple sent" << endl;
+        }
+        return EOS;
+    }
+};
+
 // DotProduct_Worker
 // Take the tuple (v1, v2, start, ending)
 /*!
@@ -195,8 +266,8 @@ struct DotProduct_Worker: ff_node_t<tuple<vector_d, vector_d, int>, double>{
         const int size = std::get<2>(*task);
 
         double partial_result = PartialDotProduct(v1, v2, size);
+        cout << "Partial result: " << partial_result << endl;
         ff_send_out(new double(partial_result)); // send the result
-
         return GO_ON;
     }
 
@@ -210,9 +281,9 @@ struct DotProduct_Worker: ff_node_t<tuple<vector_d, vector_d, int>, double>{
        \n Call DotProduct_Worker for calculate each sub dot product on this sub vectors
        \n Take all the results and calculate cbrt(sum) and then return the value
 */
-struct DotProduct_Stage: ff_node_t<tuple<vector_d, vector_d, matrix_d, uint16_t, uint16_t, uint16_t, int, int>, double> {
+struct DotProduct_Stage: ff_node_t<tuple_dot_product, double> {
     // Take the vectors (v1, v2) created on the previous stage
-    double* svc(tuple<vector_d, vector_d, matrix_d, uint16_t, uint16_t, uint16_t, int, int> *task){
+    double* svc(tuple_dot_product *task){
         const vector_d& v1 = std::get<0>(*task);
         const vector_d& v2 = std::get<1>(*task);
         matrix_d& M = std::get<2>(*task);
@@ -224,34 +295,39 @@ struct DotProduct_Stage: ff_node_t<tuple<vector_d, vector_d, matrix_d, uint16_t,
 
 
         vector<unique_ptr<ff_node>> workers;
-        //
-        list<vector_d> sub_v1_list(D);
-        list<vector_d> sub_v2_list(D);
-        //
+
         for(uint16_t w = 0; w < D; w++){
-                workers.push_back(make_unique<DotProduct_Worker>());
+            workers.push_back(make_unique<DotProduct_Worker>());
         }
 
-        ff_Farm<double> farm(move(workers), new Sink(M, i, j));
+        Sink sink(M, i, j);                             // Create the sink
+        DotProduct_Emitter emitter(K, W, D, v1, v2); // Create the emitter
+        ff_Farm<tuple<vector_d, vector_d, int>> farm(move(workers));
+        // Add the emitter and the sink
+        farm.add_emitter(emitter);
+        farm.add_collector(sink);
+        cout << "Farm created (DotProduct)" << endl;
 
-        // if K is less or equal with D we can have 1:(1,1) 
-        // [one workers:for one elememt] for calculate the dot product
-        sub_v1_list = SplitVector(v1, D, K);
-        sub_v2_list = SplitVector(v2, D, K);
-
-        farm.wrap_around();
+        //farm.wrap_around();
         farm.set_scheduling_ondemand();
-        farm.run_then_freeze();
-
-        auto begin_v1 = sub_v1_list.begin();
-        auto begin_v2 = sub_v2_list.begin();
-        
-        for(uint16_t w = 0; w < D; w++, begin_v1++, begin_v2++){
-            const uint16_t size = begin_v1->size(); // v1 and v2 have the same size
-            farm.ff_send_out(new tuple<vector_d, vector_d, int>(*begin_v1, *begin_v2, size));
+        if (farm.run_and_wait_end() < 0){
+            error("Running farm (DotProduct)\n");
+            return EOS;
         }
 
-        farm.wait();
+        // auto begin_v1 = sub_v1_list.begin();
+        // auto begin_v2 = sub_v2_list.begin();
+        
+        // for(uint16_t w = 0; w < D; w++, begin_v1++, begin_v2++){
+        //     // Take the sub-vectors and send them to the workers
+        //     const uint16_t size = begin_v1->size(); // v1 and v2 have the same size
+        //     farm.ff_send_out(new tuple<vector_d, vector_d, int>(*begin_v1, *begin_v2, size));
+        //     cout << "Tuple sent (DotProduct)" << endl;
+        // }
+
+        // farm.wait();
+        // delete task;
+        return GO_ON;
     }
 };
 
@@ -261,26 +337,49 @@ struct M_Diagonal_Stage: ff_node_t<matrix_d, matrix_d> { // Take matrix M and re
 
     M_Diagonal_Stage(uint16_t N, uint16_t K, uint16_t W, uint16_t Z, uint16_t D): N(N), K(K), W(W), Z(Z), D(D) {}
 
-    matrix_d *svc(matrix_d *M){
+    matrix_d *svc(matrix_d *M) {
         
         vector<unique_ptr<ff_node>> workers;
         // Cycle with m with m = [0, n-k[
         for (uint16_t w = 0; w < Z; w++){
+            cout << "Worker - M: " << w << endl;
             workers.push_back(make_unique<DotProduct_Stage>());
         }
-
-        ff_Farm<double> farm(move(workers));
-
-        for (uint16_t m = 0 ; m < N - K; m++){
-            // Take the vectors v1 and v2 starting from position m and m+K to K
-            vector_d v1((*M)[m].begin(), (*M)[m].begin() + K);
-            vector_d v2((*M)[m+K].begin(), (*M)[m+K].begin() + m + K);
-            tuple_dot_product task = make_tuple(v1, v2, *M, K, W, D, m, m+K);
-            farm.ff_send_out(new tuple_dot_product(task));
+        Diagonal_Emitter emitter(*M, N, K, W, D);
+        ff_Farm<tuple_dot_product, matrix_d> farm(move(workers), emitter);
+        cout << "Farm created (M-Diagonal)" << endl;
+        farm.wrap_around();
+        //farm.remove_collector();
+        farm.set_scheduling_ondemand();
+        
+        if (farm.run_and_wait_end() < 0){
+            error("Running farm (M-Diagonal)\n");
+            return EOS;
         }
 
-        farm.run_then_freeze();
-        farm.wait();
+        // cout << "Farm runned (M-Diagonal)" << endl;
+
+        // for (uint16_t m = 0 ; m < N - K; m++){
+        //     cout << "Taking v1 and v2 vectors for m: " << m << endl;
+        //     vector_d v1;
+        //     vector_d v2;
+        //     // Take the vectors v1 and v2 starting from position m and m+K to K
+        //     // Fill the v1 vector
+        //     for (uint16_t i = 0; i < K; i++){
+        //         v1.push_back((*M)[m][i]);
+        //         cout << "M[" << m << "][" << i << "]: " << (*M)[m][i] << endl;
+        //     }
+        //     for (uint16_t i = 0; i < K; i++){
+        //         v2.push_back((*M)[m+K][i]);
+        //         cout << "M[" << i << "][" << m+K << "]: " << (*M)[i][m+K] << endl;
+        //     }
+        //     cout << "Sending the tuple to the farm" << endl;
+        //     auto task = new tuple_dot_product(v1, v2, *M, K, W, D, m, m+K);
+        //     farm.ff_send_out(task);
+        //     cout << "Tuple sent" << endl;
+        // }
+        // farm.wait();
+        // delete M;
 
         SaveMatrixToFile(*M, N, "matrix_prime.txt");
 
@@ -322,10 +421,10 @@ int main(int argc, char* arg[]){
         return -1;
     }
     cout << "End" << endl;
-    /*int task = 0;
-    s1.svc(&task);
+    //int task = 0;
+    //s1.svc(&task);
     ffTime(STOP_TIME);
-    cout << "Time: " << ffTime(GET_TIME)/1000.0 << endl;*/
+    cout << "Time: " << ffTime(GET_TIME)/1000.0 << endl;
 
     // CREATE MATRIX M (Stage 1)
     // STAGE 1 create all subelements of the matrix for the diagoanl

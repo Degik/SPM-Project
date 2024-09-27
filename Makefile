bdbd64898843e34d3f967acbcaf9449f55d98e33
Degik <prodax9900@gmail.com>
2024-09-28 01:28:41 +0200

    Adding the avx sequential version

--- Makefile
+++ Makefile
@@ -4,9 +4,11 @@ MPICXX = mpic++
 CXXFLAGS = -std=c++20 -w -pthread
 INCLUDES = -I ../fastflow/
 OPTFLAGS = -O3
+ADDFLAGS = -march=native -ffast-math
+AVXFLAGS = -mavx
 
 # Targets
-TARGETS = wavefront_pf wavefront_pf_cache wavefront_farm wavefront_seq wavefront_mpi wavefront_seq_cache
+TARGETS = wavefront_pf wavefront_pf_cache wavefront_farm wavefront_seq wavefront_mpi wavefront_seq_cache wavefront_seq_avx
 
 # Source files
 SRC_PF = wavefront_pf.cpp
@@ -15,6 +17,7 @@ SRC_FARM = wavefront_farm.cpp
 SRC_SEQ = wavefront_seq.cpp
 SRC_MPI = wavefront_mpi.cpp
 SRC_SEQCACHE = wavefront_seq_cache.cpp
+SRC_SEQAVX = wavefront_seq_avx.cpp
 
 # Default target
 all: $(TARGETS)
@@ -22,13 +25,13 @@ all: $(TARGETS)
 
 # Rules for each target
 wavefront_pf: $(SRC_PF)
-	$(CXX) $(SRC_PF) -o $@ $(CXXFLAGS) $(INCLUDES)
+	$(CXX) $(SRC_PF) -o $@ $(CXXFLAGS) $(INCLUDES) $(ADDFLAGS)
 
 wavefront_pf_cache: $(SRC_PFCACHE)
-	$(CXX) $(SRC_PFCACHE) -o $@ $(CXXFLAGS) $(INCLUDES) $(OPTFLAGS)
+	$(CXX) $(SRC_PFCACHE) -o $@ $(CXXFLAGS) $(INCLUDES) $(OPTFLAGS) $(ADDFLAGS)
 
 wavefront_farm: $(SRC_FARM)
-	$(CXX) $(SRC_FARM) -o $@ $(CXXFLAGS) $(INCLUDES) $(OPTFLAGS)
+	$(CXX) $(SRC_FARM) -o $@ $(CXXFLAGS) $(INCLUDES) $(OPTFLAGS) $(ADDFLAGS)
 
 wavefront_seq: $(SRC_SEQ)
 	$(CXX) $(SRC_SEQ) -o $@ $(CXXFLAGS)
@@ -38,6 +41,9 @@ wavefront_mpi: $(SRC_MPI)
 
 wavefront_seq_cache: $(SRC_SEQCACHE)
 	$(CXX) $(SRC_SEQCACHE) -o $@ $(CXXFLAGS)
+
+wavefront_seq_avx: $(SRC_SEQAVX)
+	$(CXX) $(SRC_SEQAVX) -o $@ $(CXXFLAGS) $(AVXFLAGS)
 
 # Clean target
 clean:
---
+++ wavefront_seq_avx.cpp
@@ -0,0 +1,128 @@
+#include <cmath>
+#include <vector>
+#include <chrono>
+#include <iomanip>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <immintrin.h>
+
+//#define DEBUG
+
+using vector_d = std::vector<double>;
+
+/*!
+    \name SaveMatrixPtrToFile
+    \param M shared_ptr<vector_d> M
+    \param N uint16_t N
+    \param filename string filename
+    \brief Save the matrix M to a file
+    \note Save the matrix M to a file with the name filename
+*/
+void SaveMatrixToFile(vector_d *M, uint16_t N, std::string filename){
+    std::ofstream file;
+    file.open(filename);
+    file << std::fixed << std::setprecision(6);
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            file << (*M)[i*N+j] << " ";  // M[i][j]
+        }
+        file << std::endl;
+    }
+    file.close();
+}
+
+void CreateMatrix(vector_d &M, uint16_t N){
+    // Crete the value
+    __m256d zero = _mm256_set1_pd(0.0);
+    //
+    // https://www.intel.com/content/www/us/en/docs/intrinsics-guide/index.html#ig_expand=10,0,5812,6534&text=_mm256_storeu_pd
+    for(int i = 0; i < N * N; i+=4){
+        _mm256_storeu_pd(&M[i], zero);
+    }
+}
+
+void FillMatrix(vector_d *M, uint16_t N){
+
+    for(int m = 0; m < N; m++){
+        (*M)[m*N+m] = static_cast<double>(m+1)/N; // M[m][m] = (m+1)/N
+    }
+}
+
+void ComputeWavefrontAVX(vector_d *M, uint16_t N){
+    for (int k = 1; k < N; k++){
+        for(int m = 0; m < N-k; m++){
+            int row = m*N;
+            int col_t = (m+k)*N;
+            double element = 0.0;
+
+            int i = 0;
+            // Initialize sum_vec to zero
+            __m256d sum_vec = _mm256_setzero_pd();
+            // Process 4 elements at a time
+            for (; i <= k - 4; i += 4){
+                __m256d vec1 = _mm256_loadu_pd(&(*M)[row + i + m]);         // Load 4 elements from the row
+                __m256d vec2 = _mm256_loadu_pd(&(*M)[col_t + i + m + 1]);   // Load 4 elements from the column_transpose
+                __m256d prod = _mm256_mul_pd(vec1, vec2);                   // Multiply the two vectors
+                sum_vec = _mm256_add_pd(sum_vec, prod);                     // Add the result to the sum
+            }
+            // Extract the sum from the vector
+            __m128d sum_high = _mm256_extractf128_pd(sum_vec, 1);           // Extract the high 128 bits - Convert to 128 bit
+            __m128d sum_low = _mm256_castpd256_pd128(sum_vec);              // Cast to 128 bit vector (low)
+            __m128d sum_128 = _mm_add_pd(sum_low, sum_high);                // Add the two 128 bit
+            sum_128 = _mm_hadd_pd(sum_128, sum_128);                        // Horizontal add
+            double sum;
+            _mm_store_sd(&sum, sum_128);
+            element += sum;
+
+            // Process the remaining elements
+            for (; i < k; i++){
+                element += (*M)[row + i + m] * (*M)[col_t + i + m + 1];
+            }
+
+            double new_element = std::cbrt(element);
+            (*M)[row + m + k] = new_element;        // Update the element
+            (*M)[col_t + m] = new_element;          // Update the element for the transpose matrix
+        }
+    }
+}
+
+
+int main(int argc, char *argv[]){
+    // N
+    if (argc != 2){
+        std::cout << "Usage: " << argv[0] << "N (Size N*N)" << std::endl;
+        return -1;
+    }
+
+    uint16_t N = atoi(argv[1]);
+    
+    auto start_timer = std::chrono::high_resolution_clock::now();
+
+    vector_d M(N*N);
+
+    CreateMatrix(M, N);
+    FillMatrix(&M, N);
+    
+    #ifdef DEBUG
+        SaveMatrixToFile(&M, N, "wavefront_seq_avx_normal.txt");
+    #endif
+
+    auto stop_timer = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> time = stop_timer - start_timer;
+    std::cout << "Matrix created and filled in: " << time.count() << " seconds" << std::endl;
+
+    start_timer = std::chrono::high_resolution_clock::now();
+    //
+    ComputeWavefrontAVX(&M, N);
+    //
+    #ifdef DEBUG
+        SaveMatrixToFile(&M, N, "wavefront_seq_avx_results.txt");
+    #endif
+    //
+    stop_timer = std::chrono::high_resolution_clock::now();
+    time = stop_timer - start_timer;
+    std::cout << "Time passed to calculate the wavefront: " << time.count() << " seconds" << std::endl;
+    
+    return 0;
+}
